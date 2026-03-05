9017a9c3a4ff02623a5b171a20f8b383d5b43159
Lucas Pearson <catdevman@gmail.com>
2026-03-04 21:27:08 -0500

    solution

--- go/maxproduct/maxproduct.go
+++ go/maxproduct/maxproduct.go
@@ -1,5 +1,15 @@
 package maxproduct
 
+import "sort"
+
 func MaxProduct(nums []int) int {
-	return 0
+	sort.Ints(nums)
+	n := len(nums)
+	// Either top 3, or two most-negative × largest positive
+	a := nums[n-1] * nums[n-2] * nums[n-3]
+	b := nums[0] * nums[1] * nums[n-1]
+	if a > b {
+		return a
+	}
+	return b
 }
